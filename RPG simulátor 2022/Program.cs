1bf1ad85dc55d0b052ce8244641cdbc64666e2c9
Daniel Duda <daniel.duda@educhem.cz>
2022-03-21 13:26:42 +0100

    Přidejte soubory projektu.

---
+++ RPG simulátor 2022/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace RPG_simulátor_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vytvoření statistik DAN
+            int unavenost = 0;
+            int level = 1;
+            Dictionary<string, int> stats = new Dictionary<string, int>();
+            stats.Add("STR", 10);
+            stats.Add("DEX", 10);
+            stats.Add("INT", 10);
+
+            //pole atributu karel
+            string[] pole = { "STR", "DEX", "INT" };
+
+
+
+            //Určí jméno Vítek
+            Console.WriteLine("Zadej jméno charaktera: ");
+            string jmeno = Console.ReadLine();
+
+            do
+            {
+                // vypsání ui karel
+                Console.Clear();
+                Console.WriteLine($"Jsi unavený na {unavenost}%");
+                Console.WriteLine("chcete jít na quest nebo spánek ? q/s");
+
+                //spanek nebo quest Dan
+                if (Console.ReadLine() == "s")
+                {
+                    Console.Clear();
+                    Console.WriteLine($"{jmeno} bude spát jestě {unavenost} sekund.");
+                    Thread.Sleep(unavenost * 1000);
+                    Console.WriteLine($"{jmeno} už je vzhůru.");
+                    unavenost = 0;
+                    Console.WriteLine("Pro pokračování stiskni libovolné tlačítko...");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                    // Určí stat questy Vítek
+                    Random random = new Random();
+                    Random rnd = new Random();
+                    if (random.Next(0, 11) == 7)
+                    {
+                        Console.WriteLine($"{jmeno} zemřel.");
+                        Console.WriteLine("Pro pokračování stiskni libovolné tlačítko...");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else
+                    {
+                        string quest = pole[random.Next(0, pole.Length)];
+                        Console.WriteLine($"Jdeš na quest na {quest}, vrať se za {level * 10} sekund.\n...");
+                        Thread.Sleep(level * 1000);
+
+                        level++;
+                        stats[quest] += 10;
+                        Console.WriteLine($"{jmeno} se vrátil z questu jako rybička.\n---------------------------------  ");
+                        Console.WriteLine($"{jmeno} má lvl {level} a vlastnosti:");
+                        // vypsání kolekce
+                        foreach (var item in stats)
+                        {
+                            Console.WriteLine($"{item.Key}: {item.Value}");
+                        }
+                        unavenost += 10;
+                        Console.WriteLine("Pro pokračování stiskni libovolné tlačítko...");
+                        Console.ReadKey();
+                    }
+                }
+
+            } while (true);
+
+        }
+    }
+}
